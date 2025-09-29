8b9aab74d5e92574a3789cdfb94503f7259dc5c0
mgb111 <manishbhanushali1101@gmail.com>
2025-09-29 14:27:08 +0530

    Update site-chrome.js

--- site-chrome.js
+++ site-chrome.js
@@ -30,17 +30,18 @@ function renderFooter() {
     <footer class="bg-blue-900 text-blue-100">
       <div class="max-w-7xl mx-auto px-6">
         <div class="grid md:grid-cols-3 gap-12 py-16">
+          <div>
             <div class="flex items-center mb-6">
               <img src="majorbean-logoform.png" alt="majorbeam logo" class="mr-3 h-10 w-10 object-contain">
               <img src="majorbeam logotype.png" alt="majorbeam logotype" class="h-8 md:h-10 mr-3 object-contain hidden sm:inline-block">
             </div>
             <p class="mb-6">Creating immersive experiences at the intersection of VR and AR technology to transform how businesses connect with their audiences.</p>
-                <div class="flex space-x-4 hidden">
-                  <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-linkedin text-xl"></i></a>
-                  <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-twitter text-xl"></i></a>
-                  <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-instagram text-xl"></i></a>
-                  <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-youtube text-xl"></i></a>
-                </div>
+            <div class="flex space-x-4 hidden">
+              <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-linkedin text-xl"></i></a>
+              <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-twitter text-xl"></i></a>
+              <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-instagram text-xl"></i></a>
+              <a href="#" class="text-blue-300 hover:text-white transition duration-300"><i class="fab fa-youtube text-xl"></i></a>
+            </div>
           </div>
 
           <div>
