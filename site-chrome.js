b26d0fa4f1552ad65d4ec02c1e0ea8c3bfc9b248
mgb111 <manishbhanushali1101@gmail.com>
2026-05-05 12:59:35 +0530

    c

--- site-chrome.js
+++ site-chrome.js
@@ -16,7 +16,6 @@ function renderHeader() {
         </button>
         <nav id="navigation" class="hidden w-full md:flex md:w-auto md:space-x-6 mt-4 md:mt-0">
           <a href="index.html" class="block md:inline-block py-2 px-4 text-white hover:text-blue-200 font-medium transition duration-300">Home</a>
-          <a href="use-cases.html" class="block md:inline-block py-2 px-4 text-white hover:text-blue-200 font-medium transition duration-300">Solutions</a>
           <a href="index.html#about" class="block md:inline-block py-2 px-4 text-white hover:text-blue-200 font-medium transition duration-300">About Us</a>
           <a href="index.html#contact" class="block md:inline-block py-2 px-4 text-white hover:text-blue-200 font-medium transition duration-300">Contact</a>
         </nav>
@@ -56,7 +55,6 @@ function renderFooter() {
           <div class="text-right justify-self-end order-3 md:order-2 md:text-left md:justify-self-auto">
             <h4 class="text-lg font-semibold mb-6 text-white">Quick Links</h4>
             <ul class="space-y-3 inline-block text-right md:text-left">
-              <li><a href="use-cases.html" class="hover:text-white transition duration-300">Solutions</a></li>
               <li><a href="index.html#about" class="hover:text-white transition duration-300">About Us</a></li>
               <li><a href="index.html#contact" class="hover:text-white transition duration-300">Contact</a></li>
             </ul>
